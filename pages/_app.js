f9f7dcdedb990865102e94cafb2e5a29baab4143
Ganning Xu <ganningxu@gmail.com>
2022-01-13 21:56:10 -0500

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,41 @@
 import '../styles/globals.css'
+import theme from '../styles/theme'
+import * as React from 'react'
+import { ChakraProvider } from '@chakra-ui/react'
+import { Global, css } from '@emotion/react'
+import { useColorMode, ColorModeProvider, ColorModeScript } from '@chakra-ui/react'
+
+const GlobalStyle = ({ children }) => {
+  let { colorMode } = useColorMode()
+  return (
+    <>
+      <Global
+        styles={css`
+          html {
+            min-width: 356px;
+            scroll-behavior: smooth;
+          }
+          #__next {
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+            background: #fafafa;
+          }
+        `}
+      />
+      {children}
+    </>
+  )
+}
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <GlobalStyle>
+        <Component {...pageProps} />
+      </GlobalStyle>
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
---
+++ styles/theme.js
@@ -0,0 +1,9 @@
+import { extendTheme } from "@chakra-ui/react"
+
+const theme = extendTheme({
+    initalColorMode: 'light',
+    useSystemColorMode: false,
+})
+
+
+export default theme
