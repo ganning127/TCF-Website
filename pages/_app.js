653acc16acb4818e4abd341ca215c7b392c84d8f
Ganning Xu <ganningxu@gmail.com>
2022-01-14 14:37:30 -0500

    force light mode and update colors

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,22 @@
 import '../styles/globals.css'
 import theme from '../styles/theme'
 import * as React from 'react'
+import { useEffect } from 'react'
 import { ChakraProvider } from '@chakra-ui/react'
 import { Global, css } from '@emotion/react'
-import { useColorMode, ColorModeProvider, ColorModeScript } from '@chakra-ui/react'
+import { useColorMode } from '@chakra-ui/react'
+
+function ForceLightMode({ children }) {
+  // force light mode b/c of ChakraUI bug
+  const { colorMode, toggleColorMode } = useColorMode();
+
+  useEffect(() => {
+    if (colorMode === "light") return;
+    toggleColorMode();
+  }, [colorMode]);
+
+  return children;
+}
 
 const GlobalStyle = ({ children }) => {
   let { colorMode } = useColorMode()
@@ -32,7 +45,9 @@ function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
       <GlobalStyle>
-        <Component {...pageProps} />
+        <ForceLightMode>
+          <Component {...pageProps} />
+        </ForceLightMode>
       </GlobalStyle>
     </ChakraProvider>
   )
--- styles/theme.js
+++ styles/theme.js
@@ -1,9 +1,39 @@
 import { extendTheme } from "@chakra-ui/react"
 
-const theme = extendTheme({
+const config = {
+    colors: {
+        bkg: {
+            "light": "#ffffff"
+        },
+        text: {
+            "dark": "#444343"
+        },
+        red: {
+            "shade": "#BA2828"
+        },
+        orange: {
+            "shade": "#F9B508"
+        },
+        yellow: {
+            "shade": "#FFE600"
+        },
+        blue: {
+            "shade": "#4198B4",
+            "darkShade": "#5B6FB4"
+        },
+        green: {
+            "shade": "#2AAD4F"
+        },
+        gray: {
+            "shade": "#3C4949"
+        }
+
+    },
     initalColorMode: 'light',
-    useSystemColorMode: false,
-})
+    useSystemColorMode: false
+}
+
+const theme = extendTheme({ config })
 
 
 export default theme
