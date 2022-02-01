c6ab169995dc0802b1003cb52ed0c1135b53179d
Ganning Xu <ganningxu@gmail.com>
2022-01-31 22:58:35 -0500

    abstraction

--- components/TwoCol/TwoColCards.jsx
+++ components/TwoCol/TwoColCards.jsx
@@ -1,28 +1,16 @@
 import { SimpleGrid, Box, Text, Img, Link } from '@chakra-ui/react'
+import { EventCard } from '../Cards/EventCard'
 
-export const TwoColCards = (json) => {
-    const data = require(json);
+export const TwoColCards = ({ data }) => {
     return (
-        <SimpleGrid columns={{ base: 1, md: 2 }} spacing={20} mx="auto" textAlign="center" justifyContent="center">
-            <Box bg="#FDFDFD" shadow="lg" rounded="xl" maxW="400px" p={5} mx="auto">
-                <Img src={data[0].img} alt="pic"/>
-                <Text color="text.dark" fontSize="md" my="3" >{data[0].date}</Text>
-                <Text color="text.dark" fontSize="xl" my="3" >{data[0].title}</Text>
-                <Text color="text.dark" fontSize="md" my="3" >Description</Text>
-                <Text color="text.dark" fontSize="md" my="3" >{data[0].desc}</Text>
-                <Text color="text.dark" fontSize="md" my="3" >{data[0].info}</Text>
-                <Link href={data[0].link} color="blue.shade">Learn More</Link>
-            </Box>
+        <SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} spacing={20} mx="auto" textAlign="center" justifyContent="center">
 
-            <Box bg="#FDFDFD" shadow="lg" rounded="xl" maxW="400px" p={5} mx="auto">
-                <Img src={data[1].img} alt="pic"/>
-                <Text color="text.dark" fontSize="md" my="3" >{data[1].date}</Text>
-                <Text color="text.dark" fontSize="xl" my="3" >{data[1].title}</Text>
-                <Text color="text.dark" fontSize="md" my="3" >Description</Text>
-                <Text color="text.dark" fontSize="md" my="3" >{data[1].desc}</Text>
-                <Text color="text.dark" fontSize="md" my="3" >{data[1].info}</Text>
-                <Link href={data[1].link} color="blue.shade">Learn More</Link>
-            </Box>
+
+            {data.map((item, i) => {
+                return (
+                    <EventCard key={i} pic={item.img} title={item.title} desc={item.desc} link={item.link} price={item.price} time={item.time} />
+                )
+            })}
         </SimpleGrid>
     )
 }
