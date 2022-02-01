186796edca95761eae955220b0cb0c5e73bb3305
Ganning Xu <ganningxu@gmail.com>
2022-02-01 18:38:58 -0500

    update sub form

--- components/Footer/SubscribeForm.tsx
+++ components/Footer/SubscribeForm.tsx
@@ -6,6 +6,7 @@ import {
   Stack,
   Text,
   useColorModeValue,
+  Spinner
 } from '@chakra-ui/react'
 import { Formik, Form } from "formik";
 import React, { useState } from "react"
@@ -17,62 +18,69 @@ export const SubscribeForm = (props: HTMLChakraProps<'form'>) => {
 
 
   const subscribeEmail = async event => {
-      event.preventDefault();
-      let email = document.getElementById('emailFooter').value;
+    event.preventDefault();
+    let email = document.getElementById('emailFooter').value;
 
-      setLoading(true);
+    setLoading(true);
 
-      const res = await fetch(`/api/subscribe`,
-          {
-              body: JSON.stringify({
-                  email: email,
-                  sheetType: 'general'
-              }),
-              headers: {
-                  'Content-Type': 'application/json',
-                  'Accept': 'application/json'
-              },
-              method: 'POST'
-          }
-      )
+    const res = await fetch(`/api/subscribe`,
+      {
+        body: JSON.stringify({
+          email: email,
+        }),
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        },
+        method: 'POST'
+      }
+    )
 
-      console.log(`Subscribing ${email} to newsletter...`)
-      let result = await res.json()
-      setSubmitted(true);
+    console.log(`Subscribing ${email} to newsletter...`)
+    let result = await res.json()
+    setSubmitted(true);
+    setLoading(false);
   }
   return (
-    <chakra.form {...props} onSubmit={subscribeEmail}>
+    <>     <chakra.form {...props} onSubmit={subscribeEmail}>
       <Stack spacing="4">
         <FooterHeading color="text.dark">Subscribe to our newsletter</FooterHeading>
-        <Text>Never miss another event from TCF!</Text>
-        <Stack spacing="4" direction={{ base: 'column', md: 'row' }} >
-          <Input
-            id="emailFooter"
-            bg={useColorModeValue('white', 'inherit')}
-            placeholder="Enter your email"
-            type="email"
-            required
-            focusBorderColor="blue.shade"
-            _placeholder={{
-              opacity: 1,
-              color: useColorModeValue('gray.500', 'whiteAlpha.700'),
-            }}
-          />
-          <Button
-            type="submit"
-            // colorScheme="blue"
-            bg="blue.shade"
-            _hover={{
-              bg: 'blue.shade.hover',
-            }}
-            color="white"
-            flexShrink={0}
-            width={{ base: 'full', md: 'auto' }}
-          >
-            Subscribe
-          </Button>
-        </Stack>
+        {!submitted && <>
+          <Text>Never miss another event from TCF!</Text>
+          <Stack spacing="4" direction={{ base: 'column', md: 'row' }} >
+            <Input
+              id="emailFooter"
+              placeholder="Enter your email"
+              type="email"
+              required
+              focusBorderColor="blue.shade"
+              _placeholder={{
+                opacity: 1,
+                color: 'gray.500',
+              }}
+            />
+            <Button
+              type="submit"
+              // colorScheme="blue"
+              bg="blue.shade"
+              _hover={{
+                bg: 'blue.shade.hover',
+              }}
+              color="white"
+              flexShrink={0}
+              width={{ base: 'full', md: 'auto' }}
+            >
+
+              {loading &&
+                <Spinner mr="2" />}
+
+              Subscribe
+            </Button>
+          </Stack>
+        </>}
+        {submitted && <Text>Thanks for subscribing, well see you soon 👋</Text>}
       </Stack>
     </chakra.form>
+    </>
   )
 }
