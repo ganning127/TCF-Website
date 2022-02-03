b9439e048c820b5e96bb7e8688fbea2793b8f011
Ganning Xu <ganningxu@gmail.com>
2022-02-03 11:37:08 -0500

    update content

--- pages/api/sendEmail.js
+++ pages/api/sendEmail.js
@@ -2,8 +2,9 @@ import nodemailer from 'nodemailer';
 
 export default async function handler(req, res) {
     const fromEmail = '"TCF Contact" <contact@thecodingfoundation.com>';
-    const toEmail = 'ganningxu@gmail.com'
+
     const request = JSON.parse(req.body);
+    const toEmail = request['email']
 
     // create reusable transporter object using the default SMTP transport
     let transporter = nodemailer.createTransport({
@@ -14,12 +15,19 @@ export default async function handler(req, res) {
         }
     });
 
+    await transporter.sendMail({
+        from: fromEmail, // sender address
+        to: "thecodingfoundation@gmail.com", // list of receivers
+        subject: request["subject"], // Subject line
+        text: `New Inquiry from ${toEmail}: ${request["message"]}`, // plain text body
+    });
+
     // send mail with defined transport object
     let info = await transporter.sendMail({
         from: fromEmail, // sender address
         to: toEmail, // list of receivers
-        subject: request["subject"], // Subject line
-        text: request["message"], // plain text body
+        subject: "TCF Message Received", // Subject line
+        text: "Thank you for contacting The Coding Foundation. We have received your request and will get back to you as soon as possible!", // plain text body
     });
 
 
