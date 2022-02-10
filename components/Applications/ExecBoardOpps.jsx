8b8fdf14f4b736f27e6cc882a92e8759addc1cdb
Asia <asiamon25@gmail.com>
2022-02-09 19:01:36 -0700

    fixed spelling mistake

--- components/Applications/ExecBoardOpps.jsx
+++ components/Applications/ExecBoardOpps.jsx
@@ -7,8 +7,8 @@ export const ExecBoardOpps = ({ }) => {
             <Text fontWeight="normal" color="text.dark" fontSize="2xl" my="3" >Looking for leadership positions that look great on your resume, college applications?</Text>
             <Text fontWeight="normal" color="text.dark" fontSize="2xl" my="3" >Why wait? Apply now to become an executive board memeber of The Coding Foundation Family!</Text>
             <Text fontWeight="normal" color="text.dark" fontSize="2xl" my="3" >Please note that since we are a student run organization that all positions are UNPAID. With that being said, you will have the opportunity to work in a corporate-simulated environment, recieve Linkedin endorsements, volunteer hours, referrals (if requested), and much more!</Text>
-            <Text fontWeight="normal" color="text.dark" fontSize="2xl" my="3" >Time commitment and flexibility: We understand the time commitments surrounding your school work, and will provide flexibility in certain timings. You will be able to complete tasks at your own time, and schedule meeting accordingly.</Text>
-            <Text fontWeight="normal" color="text.dark" fontSize="2xl" my="3" >Departments: The role will include holding department meetings with current and newly hired interns, and establishing new initiatives into your department as well. We balue memebers with a strong sense of time management, skillset, and interpersonal relations.</Text>
+            <Text fontWeight="normal" color="text.dark" fontSize="2xl" my="3" >Time commitment and flexibility: We understand the time commitments surrounding your school work, and will provide flexibility in certain timings. You will be able to complete tasks at your own time, and schedule meetings accordingly.</Text>
+            <Text fontWeight="normal" color="text.dark" fontSize="2xl" my="3" >Departments: The role will include holding department meetings with current and newly hired interns, and establishing new initiatives into your department as well. We value memebers with a strong sense of time management, skillset, and interpersonal relations.</Text>
         </Box >
     )
 }
