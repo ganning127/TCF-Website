d84d0e627a0b1c193ba6043cb5fb9a3ddcdd00d8
Ganning Xu <ganningxu@gmail.com>
2022-02-28 10:40:33 -0500

    fix key prop error

--- components/FAQ/Faq.jsx
+++ components/FAQ/Faq.jsx
@@ -1,4 +1,4 @@
-import { Accordion, Heading, AccordionItem, Text, AccordionButton, AccordionPanel, Box, AccordionIcon} from '@chakra-ui/react'
+import { Accordion, Heading, AccordionItem, Text, AccordionButton, AccordionPanel, Box, AccordionIcon } from '@chakra-ui/react'
 
 import Posts from "../../data/faq.json"
 
@@ -9,26 +9,26 @@ export const Faq = () => {
                 <Heading as="h1" size="2xl" color="blue.shade.hover" mb="4" fontWeight="bold"> FAQ</Heading>
                 <Text fontSize='lg'>Any questions? Look here. Here are some frequently asked questions from students.</Text>
             </Box>
-            <br/>
+            <br />
             <Accordion allowToggle>
-                {Posts.map( data => {
+                {Posts.map((data, i) => {
                     return (
-                        <AccordionItem>
+                        <AccordionItem key={i}>
                             <h2>
-                            <AccordionButton _expanded={{ bg: 'blue.shade', color: 'white' }}>
-                                <Box flex='1' textAlign='left'>
-                                {data.question}
-                                </Box>
-                                <AccordionIcon />
-                            </AccordionButton>
+                                <AccordionButton _expanded={{ bg: 'blue.shade', color: 'white' }}>
+                                    <Box flex='1' textAlign='left'>
+                                        {data.question}
+                                    </Box>
+                                    <AccordionIcon />
+                                </AccordionButton>
                             </h2>
                             <AccordionPanel pb={4}>
-                            {data.answer}
+                                {data.answer}
                             </AccordionPanel>
                         </AccordionItem>
                     )
                 })}
             </Accordion>
         </>
-    )   
+    )
 }
