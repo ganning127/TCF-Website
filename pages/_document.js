c55e28e751ca44c067cd2bf4c57f446e2108d5dd
Ganning Xu <ganningxu@gmail.com>
2022-04-11 21:46:58 -0400

    fix google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -1,20 +1,36 @@
-import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
-import { ColorModeScript } from "@chakra-ui/react"
-import theme from '../styles/theme'
+import NextDocument, { Html, Head, Main, NextScript } from "next/document";
+import { ColorModeScript } from "@chakra-ui/react";
+import theme from "../styles/theme";
 
 export default class MyDocument extends NextDocument {
-
-    render() {
-        return (
-            <Html lang="en">
-                <Head>
-                </Head>
-                <body>
-                    <ColorModeScript initialColorMode={theme.initalColorMode} />
-                    <Main />
-                    <NextScript />
-                </body>
-            </Html>
-        )
-    }
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
+        </Head>
+        <body>
+          <ColorModeScript initialColorMode={theme.initalColorMode} />
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
